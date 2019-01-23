bb8551d8d7c672220bc7de31c4e701cfff26945a
zcreturn0 <237350543@qq.com>
2019-01-23 16:43:48 +0800

    use config to reduce code

---
+++ webpack/first/webpack.config.js
@@ -0,0 +1,25 @@
+const webpack = require('webpack');
+const path = require('path');
+
+module.exports = {
+    entry: ["./entry.js"],
+    output: {
+        path: path.resolve(__dirname,'dist'),
+        filename: 'bundle.js'
+    },
+    mode: 'development',
+    module: {
+        rules: [
+            {
+                test: /\.css/,
+                use: [
+                    'style-loader',
+                    'css-loader'
+                ]
+            }
+        ]
+    },
+    plugins:[
+        new webpack.BannerPlugin('头部注释')
+    ]
+}
