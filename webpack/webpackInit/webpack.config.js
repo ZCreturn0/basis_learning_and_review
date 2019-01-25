715984113c436ae6de6038fb64788258bb9f2749
zcreturn0 <237350543@qq.com>
2019-01-25 17:17:18 +0800

    add css

--- webpack/webpackInit/webpack.config.js
+++ webpack/webpackInit/webpack.config.js
@@ -5,5 +5,14 @@ module.exports = {
     output: {
         filename: 'bundle.js',
         path: path.resolve(__dirname,'./dist')
+    },
+    module:{
+        rules:[
+            {
+                test:/\.css$/,
+                // minimize:开启压缩  已失效...
+                use: ['style-loader','css-loader']
+            }
+        ]
     }
 }
